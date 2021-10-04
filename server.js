6da4547b412e83ece4305ef8447a3182ef309c98
asadbek21coder <asadbekegambergenov21coder@gmail.com>
2021-10-04 22:49:04 +0500

    init

---
+++ server.js
@@ -0,0 +1,67 @@
+const TelegramBotApi = require('node-telegram-bot-api')
+const token = "2031079570:AAGRl5AhO658RUjicHotMx4mrwN0Tretr-I"
+const bot = new TelegramBotApi(token, { polling: true })
+const chats = {}
+const {gameOptions, againOptions} = require('./options.js')
+
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, ` 0 dan 9 gacha bir raqam o'yladim`)
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    console.log(chats[chatId])
+    await bot.sendMessage(chatId, `Raqamni toping`, gameOptions)
+}
+
+const start = () => {
+    bot.setMyCommands([
+        { command: '/start', description: 'Salomlashish' },
+        { command: '/info', description: 'boshlangich malumotlar' },
+        { command: '/game', description: `son topish o'yiniz` },
+
+    ])
+
+    bot.on('message', async message => {
+        const chatId = message.chat.id
+        const text = message.text
+        const name = message.chat.first_name
+        
+        if (text === '/game') {
+            console.log(message.from.first_name + ' ' + 'game')
+            return startGame(chatId)
+            
+        }
+        
+        if (text == '/start') {
+            await console.log(message.from.first_name + ' ' + 'starting')
+            await bot.sendSticker(chatId, "https://tlgrm.eu/_/stickers/fc2/01a/fc201aad-5ea0-48fd-b779-f8a237ff21ae/2.webp")
+
+            return bot.sendMessage(chatId, `Assalomu alaykum, ${name}. Botimizga xush kelibsiz`)
+
+        }
+        if (text == '/info') {
+            await console.log(message.from.first_name + ' ' + 'info')
+            return bot.sendMessage(chatId, `ushbu bot bilan o'yin o'ynashingiz mumkin. Buning uchun /game buyrug'ini kiriting`)
+        }
+
+
+        return bot.sendMessage(chatId, 'Sizni tushunmadim')
+
+    })
+
+    bot.on("callback_query", async msg => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+        if (data == '/again') {
+            return startGame(chatId)
+        }
+        if (data == chats[chatId]) {
+            return bot.sendMessage(chatId, `Barakalla siz to'g'ri topdingiz, men o'ylagan raqam ham ${data}`, againOptions)
+        }
+        else {
+            return bot.sendMessage(chatId, `Afsuski topa olmadingiz men ${chats[chatId]} raqamini o'ylagandim`, againOptions)
+        }
+    })
+}
+
+start()
